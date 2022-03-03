9ea2a55ed38092e985f7da7e061bc03647147d41
Teddy Lelong <teddy@webalgo.fr>
2022-03-03 09:29:51 +0100

    Added templates parts files

--- inc/admin/header.inc.php
+++ inc/admin/header.inc.php
@@ -1,1 +1,1 @@
-<?php
+<h1>Interface d'administration</h1>
---
+++ inc/website/nav.inc.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="/">Accueil</a></li>
+        <li><a href="#">À propos</a></li>
+        <li><a href="/article/index/">Le Blog</a></li>
+        <li><a href="#">Me contacter</a></li>
+    </ul>
+</nav>
